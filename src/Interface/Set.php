3ce84de6b9f45b6f41ba6918cb8374d19c638871
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-07-19 10:36:24 +0300

    Добавление интерфейсов.

---
+++ src/Interface/Set.php
@@ -0,0 +1,17 @@
+<?php
+namespace Process\Interface
+{
+    /**
+     * Интерфейс процесса записи элемента или элементов.
+     */
+    interface Set
+    {
+        /**
+         * Записывает элемент, запуская при этом событийные методы процесса.
+         * @param  mixed $item Элемент или null, если следует завершить
+         *                     процесс записи.
+         * @return void
+         */
+        public function process($item);
+    }
+}
