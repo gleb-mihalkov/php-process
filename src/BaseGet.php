0b8225a0d08eb43593ca8052cc53ab098c730c3d
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-07-19 10:51:03 +0300

    Добавление двух базовых классов с новыми интерфейсами.

---
+++ src/BaseGet.php
@@ -0,0 +1,17 @@
+<?php
+namespace Process
+{
+    use Process\Interfaces\Get as GetInterface;
+
+    /**
+     * Базовый класс процесса получения элементов или элемента.
+     */
+    abstract class BaseGet extends Base implements GetInterface
+    {
+        /**
+         * Метод, получающий элемент или элементы.
+         * @return mixed Элемент или null, если элементы в кончились.
+         */
+        abstract protected function main();
+    }
+}
---
+++ src/BaseSet.php
@@ -0,0 +1,18 @@
+<?php
+namespace Process
+{
+    use Process\Interfaces\Set;
+
+    /**
+     * Базовый класс процесса записи элемента или элементов.
+     */
+    abstract class BaseSet extends Base implements SetInterface
+    {
+        /**
+         * Записывает элемент.
+         * @param  mixed $item Элемент или null, если процесс записи следует завершить.
+         * @return void
+         */
+        abstract protected function main($item);
+    }
+}
