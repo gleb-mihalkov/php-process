8452df95b943e0481f70b3eda088814cd2fa6213
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-07-22 01:59:00 +0300

    Добавление документации.

---
+++ docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Process\\Base"],["c","Process\\BaseGet"],["c","Process\\BaseSet"],["c","Process\\Exec"],["c","Process\\Fetch"],["c","Process\\Interfaces\\Get"],["c","Process\\Interfaces\\Set"],["c","Process\\Read"],["c","Process\\Write"]];
